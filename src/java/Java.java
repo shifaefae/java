314be94604cc68104f8f802c2b8cdfa59c3ce5dc
shifa <nuriyahshifaaun@gmail.com>
2024-05-15 19:26:58 +0700

    hay semua

---
+++ src/java/Java.java
@@ -0,0 +1,15 @@
+
+public class Java {
+    public static int doubleInteger(int i) {
+        
+        return i * 2;
+        
+    }
+    public static void main(String[] args) {
+        int input = 5;
+        int output = doubleInteger(input);
+        System.out.println("Bilangan awal :" + input);
+        System.out.println("Bilangan setelah digandakan " + output);
+    }
+    
+}
